f9ca8988d013fe4da9e81ae3e9f878038e371301
IT19153964 <tharushadilmith99@gmail.com>
2021-06-03 01:00:08 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+
+function App() {
+    return(
+    <div>
+        <h1>Hello React</h1>
+        <Router>
+            <section>
+                
+            </section>
+        </Router>
+        
+    </div>
+    );
+}
+
+export default App;
