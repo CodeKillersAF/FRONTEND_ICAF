5a389d94ea8a12a1dde65fa911d0c576e3307c20
kawsikan <kawsinote@gmail.com>
2021-06-06 16:46:26 +0530

    created components and pages folder

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/home'
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Navbar />
+                <section>
+                    <Switch>
+                        <Route path="/" component={Home} exact />
+                    </Switch>
+                </section>
+                {/* <Footer/> */}
+            </Router>
+        </div>
+    )
+}
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import App from './App'
 
-render(<h1>Hello to React</h1>, document.getElementById('app'));
+render(
+    <App />,
+    document.getElementById('app')
+);
