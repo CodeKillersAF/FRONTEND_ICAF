a79c4dfad7887aa7e0895ca34d160ccd179b2172
kawsikan <kawsinote@gmail.com>
2021-06-06 17:10:07 +0530

    added missing files

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Button(props) {
+
+    return (
+        <>
+            <button className={props.className} type="button">{props.name}</button>
+        </>
+
+    )
+
+};
---
+++ src/components/CardImage/CardImage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+export default function CardImage(props) {
+
+    return (
+        <>
+            <div className="col-md-6">
+                <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 
+                                position-relative">
+                    <div className="col p-4 d-flex flex-column position-static">
+                        <strong className={props.tagclassName}>{props.tag}</strong>
+                        <h3 className="mb-0">{props.title}</h3>
+                        <div className="mb-1 text-muted">{props.date}</div>
+                        <p className="card-text mb-auto">{props.content}</p>
+                        <a href="#" className="stretched-link">{props.link}</a>
+                    </div>
+                    <div className="col-auto d-none d-lg-block">
+                        <img className="container-fluid" src={props.image} alt="sample" />
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
