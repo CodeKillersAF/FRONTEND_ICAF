9c3b4622e44149e8011480ebb37c33a4514871e3
TharushaDilmith <tharushadilmith99@gmail.com>
2021-07-01 01:01:06 +0530

    push keynote functions

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
                         <Route path="/" component={Home} exact />
                     </Switch>
                 </section>
-                {/* <Footer/> */}
+                <Footer/>
             </Router>
         </div>
     )
