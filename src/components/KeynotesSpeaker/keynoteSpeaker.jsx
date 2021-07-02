63f77667db89b646279b6497e1ece1062e30ad3b
IT19153278 <it19153278@my.sliit.lk>
2021-07-02 20:09:25 +0530

    edit keynote design

--- src/components/KeynotesSpeaker/keynoteSpeaker.jsx
+++ src/components/KeynotesSpeaker/keynoteSpeaker.jsx
@@ -1,55 +1,65 @@
-import axios from '../../axios';
-import React, { useState,useEffect } from 'react';
+import axios from "../../axios";
+import React, { useState, useEffect } from "react";
 
 function keynoteSpeaker() {
-    const [keynote, setKeynotes] = useState([]);
+  const [keynote, setKeynotes] = useState([]);
 
-    async function fetchData(){
-        await axios.get('/keynotes/get-approved-keynotes')
-        .then((response)=>{
-            
-            setKeynotes(response.data.data);
-        })
-    }
+  async function fetchData() {
+    await axios.get("/keynotes/get-approved-keynotes").then((response) => {
+      setKeynotes(response.data.data);
+    });
+  }
 
-    useEffect(() => {
-        fetchData();
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    
-
-    return (
-        <div>
-                <br />
-                <section className="keynote">
-
-                    <div className="container text-center">
-                        <h1><b>Our Keynote Speakers</b></h1>
-                        <p>Lots of speakers share their knowledge</p>
-
-                        <div className="row">
-                            {keynote.map((speaker) => (
-
-                            <div className="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
-                                <figure className="figure">
-                                    <img src={speaker.speakerImageUrl}
-                                      className="rounded-circle" height="200px" width="200px" />
-
-                                      <figcaption>
-                                          <h2>{speaker.speakerName}</h2>
-                                          <p className="figure-caption">{speaker.position}</p>
-                                          <p className="figure-caption">{speaker.description}</p>
-                                      </figcaption>
-                                </figure>
-                            </div>
-
-                        ))}
-
-                        </div>
-                    </div>
-                </section>
+  return (
+    <div>
+     <div>
+        <br />
+        <br />
+        {/* <section className="sectionDesign"> */}
+        <div class="card text-center">
+            <div class="card-header">
+        <h1>
+            <b>Keynotes</b>
+        </h1>
+        </div>
+        </div>
+          <div class="text-center">
+            <div class="header">
             </div>
-    )
+            {/* <div class="card-body"> */}
+            <br />
+              <h5 class="card-title">
+                <b>Lecturer Name</b> 
+              </h5>
+              <img
+                src="https://i.ibb.co/ZhFKQ1H/lecturer.jpg"
+                class="rounded-circle"
+                width="200px"
+                height="200px"
+                alt="..."
+              ></img><br />
+              <b>Position</b><br />
+
+              <p class="card-text">
+                So, as is our hallowed duty as a literary and culture
+                website—though with full awareness of the potentially fruitless
+                and endlessly contestable nature of the task—in the coming
+                weeks.So, as is our hallowed duty as a literary and culture
+                website—though with full awareness of the potentially fruitless
+                and endlessly contestable nature of the task—in the coming
+                weeks.
+              </p>
+              <br /><br /><br />
+            </div>
+          {/* </div> */}
+          {/* </section> */}
+          </div>
+    </div>
+  );
 }
 
-export default keynoteSpeaker
+export default keynoteSpeaker;
